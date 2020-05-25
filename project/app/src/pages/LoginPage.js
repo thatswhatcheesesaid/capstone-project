29fe4b2eb9f32a190c93e075b6e971c0e800674c
Carl Hernandez <carl.hernandez89@gmail.com>
2020-05-25 14:47:41 -0600

    began Profile Page

---
+++ project/app/src/pages/LoginPage.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+
+export function LoginPage () {
+
+	return (
+
+		<>
+
+		</>
+	)}
