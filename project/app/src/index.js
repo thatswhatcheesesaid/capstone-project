2b9a772dfc26d8f451e26352a8d064bc22504371
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-01 13:04:23 -0600

    removed extra pages

--- project/app/src/index.js
+++ project/app/src/index.js
@@ -6,13 +6,12 @@ import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import { PicturePage } from './pages/PicturePage'
-import {ProfilePage} from './pages/ProfilePage'
+
 
 const Routing = () => (
   <>
     <BrowserRouter>
       <Switch>
-        {/*<Route exact path="/" component={ProfilePage}/>*/}
         <Route component={PicturePage}/>
         <Route component={Home}/>
         <Route component={FourOhFour}/>
