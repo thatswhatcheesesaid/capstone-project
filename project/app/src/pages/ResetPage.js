299f8bedcca3e027d341964f08267514d0455c75
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-01 21:37:11 -0600

    removed extra components

--- project/app/src/pages/ResetPage.js
+++ project/app/src/pages/ResetPage.js
@@ -12,24 +12,6 @@ export function ResetPage () {
 	return (
 	<>
 
-{/*This is the navbar*/}
-	<Container fluid className="px-0" style={{ backgroundColor: 'rgb(255, 137, 6)' }}>
-		<Navbar collapseOnSelect expand="lg">
-			<Navbar.Brand>
-				<span style={{fontSize: 37}}>That's What Cheese Said</span>
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="ml-auto">
-					<Button className="m-2" ><Nav.Link href="#">Restaurants</Nav.Link></Button>
-					<Button className="m-2"><Nav.Link href="#Pictures">Pictures</Nav.Link></Button>
-					<Button className="m-2"><Nav.Link href="#Log In">Log In</Nav.Link></Button>
-					<Button className="m-2"><Nav.Link href="#About Us">About Us</Nav.Link></Button>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
-	</Container>
-
 
 	<main style={{ backgroundColor: 'rgb(15, 14, 23)' }} className="py-5">
 
@@ -74,15 +56,6 @@ export function ResetPage () {
 	</Container>
 	</main>
 
-
-{/*This is the footer*/}
-	<Container fluid className= "py-3" style={{ backgroundColor: 'rgb(255, 137, 6)' }}>
-		<Row>
-			<Col>
-				<p className="text-center">Copyright info</p>
-			</Col>
-		</Row>
-	</Container>
 
 	</>
 	)
