5bad555b06132ba8a37ee779af891e3c07c70fab
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-01 21:29:48 -0600

    removed extra components

--- project/app/src/pages/PicturePage.js
+++ project/app/src/pages/PicturePage.js
@@ -34,27 +34,6 @@ export function PicturePage() {
 	`}
 </style>
 
-{/*This is the navbar*/}
-	<Container fluid className="px-0" style={{ backgroundColor: 'rgb(255, 137, 6)' }}>
-		<Navbar collapseOnSelect expand="lg">
-			<Navbar.Brand>
-				<span style={{fontSize: 37}}>That's What Cheese Said</span>
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="ml-auto">
-					{/*<Nav.Link href="#">Restaurants</Nav.Link>*/}
-					{/*<Nav.Link href="#Pictures">Pictures</Nav.Link>*/}
-					{/*<Nav.Link href="#Log In">Log In</Nav.Link>*/}
-					{/*<Nav.Link href="#About Us">About Us</Nav.Link>*/}
-					<Button className="m-2"><Nav.Link href="#">Restaurants</Nav.Link></Button>
-					<Button className="m-2"><Nav.Link href="#Pictures">Pictures</Nav.Link></Button>
-					<Button className="m-2"><Nav.Link href="#Log In">Log In</Nav.Link></Button>
-					<Button className="m-2"><Nav.Link href="#About Us">About Us</Nav.Link></Button>
-				</Nav>
-		</Navbar.Collapse>
-		</Navbar>
-	</Container>
 
 
 {/*This is the body of the page*/}
@@ -125,14 +104,6 @@ export function PicturePage() {
 
 </main>
 
-{/*This is the footer*/}
-	<Container fluid className= "py-3" style={{ backgroundColor: 'rgb(255, 137, 6)' }}>
-		<Row>
-			<Col>
-				<p className="text-center">Copyright info</p>
-			</Col>
-		</Row>
-	</Container>
 
 		</>
 	)}
