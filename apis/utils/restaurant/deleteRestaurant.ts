d8d9048a6efe0fe54811d8a780c8715ff4ade2fb
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-11 15:39:10 -0600

    added some mysql functions

---
+++ apis/utils/restaurant/deleteRestaurant.ts
@@ -0,0 +1,15 @@
+import {Restaurant} from "../interfaces/restaurant";
+import {connect} from "../..src/database";
+
+export async function insertRestaurant(restaurant: Restaurant) {
+    try {
+        const mySqlConnection = await connect()
+        const mySqlQuery ="DELETE FROM restaurant WHERE restaurantId = VALUES(UUID_TO_BIN(UUID())"
+        const [rows] = await mySqlConnection.execute(mySqlQuery, restaurant)
+        return "Restaurant deleted successfully"
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//ASK FOR HELP WITH THIS ONE TOO
