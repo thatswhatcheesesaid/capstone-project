231031d05505f81347268a06bfa253438729edb4
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-11 16:59:16 -0600

    added selectProfileByEmail

--- apis/utils/interfaces/restaurant.ts
+++
@@ -1,8 +0,0 @@
-export interface Restaurant {
-    restaurantId: string | null,
-    restaurantFeaturedPic: string,
-    restaurantFeaturedPicCaption: string,
-    restaurantGoogleLink: string,
-    restaurantName: string,
-    restaurantSocialMediaUrl: string
-}
