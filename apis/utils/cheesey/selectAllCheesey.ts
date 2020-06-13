220eab85bd1faf6f6d3b3e3f787ba8ed10649e01
Kevin Adams <kadams34@cnm.edu>
2020-06-12 21:20:01 -0600

    modified selectAllCheesey.ts

--- apis/utils/cheesey/selectAllCheesey.ts
+++ apis/utils/cheesey/selectAllCheesey.ts
@@ -5,5 +5,9 @@ export async function selectAllCheesey(cheesey: Cheesey) {
     try {
         const mySqlConnection = await connect()
         const mySqlQuery = "SELECT BIN_TO_UUID AS cheeseyFoodpicId, cheeseyProfileId, cheeseyScore FROM cheesey"
+        const [rows] = await mySqlConnection.execute(mySqlQuery, cheesey)
+        return "All cheesey's selected successfully"
+    } catch (error) {
+        console.log(error)
     }
 }
