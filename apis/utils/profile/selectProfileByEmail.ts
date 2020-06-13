66f6208fae4456e0dc3310a4269fe9d5a608d56a
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-12 21:34:35 -0600

    finished profile entity

--- apis/utils/profile/selectProfileByEmail.ts
+++ apis/utils/profile/selectProfileByEmail.ts
@@ -4,8 +4,11 @@ import {connect} from "../../src/database";
 export async function selectProfileByEmail(profileEmail: string) {
     try {
         const mySqlConnection = await connect()
+
         const mySqlQuery = "SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileEmail, profileHash, profileName FROM profile WHERE profileEmail = :profileEmail"
+
         const [rows] = await mySqlConnection.execute(mySqlQuery, profileEmail)
+
         return rows
     } catch (error) {
         console.log(error)
