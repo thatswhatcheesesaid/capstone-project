3f428ac1cafcf2a0aef1e3b18a368da4b9433f78
mattg <mattgal2002@gmail.com>
2020-06-12 21:28:57 -0600

    added getPictureByProfile function

--- apis/utils/picture/selectAllPictures.ts
+++ apis/utils/picture/selectAllPictures.ts
@@ -4,7 +4,7 @@ import{connect} from "../../src/database";
 export async function selectAllPictures(picture: Picture){
 	try {
 		const mySqlConnection = await connect()
-		const mySqlQuery = "SELECT BIN_TO_UUID(foodpicId) AS foodId, foodpicProfileId, foodpicRestaurantId, foodpicUrl FROM foodpic";
+		const mySqlQuery = "SELECT BIN_TO_UUID(foodpicId) AS foodId, foodpicProfileId, foodpicRestaurantId, foodpicCaption, foodpicUrl FROM foodpic";
 		const [rows] = await mySqlConnection.execute(mySqlQuery, picture)
 		return "All pictures selected successfully"
 	} catch (error) {
