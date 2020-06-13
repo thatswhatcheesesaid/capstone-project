80c1d2f302b3f31a03a9a82d9647194b0fa0dfe0
Kevin Adams <kadams34@cnm.edu>
2020-06-12 21:34:36 -0600

    finished fork entity

--- apis/utils/fork/selectAllFork.ts
+++
@@ -1,3 +0,0 @@
-import {Fork} from "../interfaces/fork";
-import {connect} from "../../src/database";
-
---
+++ apis/utils/fork/selectAllForks.ts
@@ -0,0 +1,15 @@
+import {Fork} from "../interfaces/fork";
+import {connect} from "../../src/database";
+
+export async function selectAllForks(fork: Fork) {
+    try {
+        const mySqlConnection = await connect()
+        const mySqlQuery = "SELECT BIN_TO_UUID AS forkProfileId, forkRestaurantId, forkScore FROM fork"
+        const [rows] = await mySqlConnection.execute(mySqlQuery, fork)
+        return "All Forks selected successfully"
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//left out () after BIN_TO_UUID on line 7 - no primary key
