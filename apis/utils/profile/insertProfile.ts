83da2ae50f12d543b8d2be4035f20c98031bc90e
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-15 17:14:48 -0600

    added sign up stuff

--- apis/utils/profile/insertProfile.ts
+++ apis/utils/profile/insertProfile.ts
@@ -7,7 +7,7 @@ export async function insertProfile(profile: Profile) {
 
         const mySqlQuery = "INSERT INTO profile(profileId, profileActivationToken, profileEmail, profileHash, profileName VALUES(UUID_TO_BIN(UUID()), :profileActivationToken, :profileEmail, :profileHash, :profileName)";
 
-        const [rows] = await mySqlConnection.execute(mySqlQuery, profile)
+        const [rows] = await mySqlConnection.execute(mySqlQuery, {profile})
 
         return "Profile added successfully"
     } catch (error) {
