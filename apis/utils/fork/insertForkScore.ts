9ca732c36ede39434e9e58d6baf6ee5b95ec7f89
Kevin Adams <kadams34@cnm.edu>
2020-06-16 14:51:14 -0600

    fixed insertForkScore

--- apis/utils/fork/insertForkScore.ts
+++ apis/utils/fork/insertForkScore.ts
@@ -4,7 +4,7 @@ import {connect} from "../../src/database";
 export async function insertForkScore(fork: Fork) {
     try {
         const mySqlConnection = await connect()
-        const mySqlQuery = "INSERT INTO fork(forkProfileId, forkRestaurantId, forkScore) VALUES(UUID_TO_BIN(UUID()), :forkScore)"
+        const mySqlQuery = "INSERT INTO fork(forkProfileId, forkRestaurantId, forkScore) VALUES (UUID_TO_BIN(:forkProfileId), UUID_TO_BIN(:forkRestaurantId), :forkScore)"
         const [rows] = await mySqlConnection.execute(mySqlQuery, fork)
         return 'ForkScore created successfully'
     } catch (error) {
