83220361fc7614100c7f1d6af2c9d0cf8eb39dfd
Kevin Adams <kadams34@cnm.edu>
2020-06-17 14:15:45 -0600

    fixed the ECONNREFUSED error finally

--- apis/src/database.ts
+++ apis/src/database.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 export async function connect (): Promise<Pool> {
 	const connection = await createPool({
-		host: 'localhost',
+		host: process.env.HOST,
 		user: process.env.MYSQL_USER,
 		password: process.env.MYSQL_PASSWORD,
 		database: process.env.MYSQL_DATABASE,
