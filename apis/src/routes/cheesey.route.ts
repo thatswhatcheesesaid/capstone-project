e38802dad3b4504b38346a573d089b6a08d3d191
Kevin Adams <kadams34@cnm.edu>
2020-06-17 19:27:12 -0600

    fixed cheesey route error

--- apis/src/routes/cheesey.route.ts
+++ apis/src/routes/cheesey.route.ts
@@ -3,6 +3,7 @@ import {
     getCheeseyByFoodpicIdAndProfileIdController,
     postCheeseyScoreController
 } from "../controllers/cheesey.controller";
+import {getForkByProfileIdAndRestaurantIdController} from "../controllers/fork.controller";
 
 
 export const CheeseyRoute = Router()
@@ -10,3 +11,5 @@ export const CheeseyRoute = Router()
 CheeseyRoute.route("/")
     .post(postCheeseyScoreController)
 
+CheeseyRoute.route("/foodpicId/:foodpicId/profileId/:profileId")
+    .get(getForkByProfileIdAndRestaurantIdController)
