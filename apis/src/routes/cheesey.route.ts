dbda648706fda0a862a152a85a7372c15a3afd9d
Kevin Adams <kadams34@cnm.edu>
2020-06-17 19:39:04 -0600

    fixed dumdum error

--- apis/src/routes/cheesey.route.ts
+++ apis/src/routes/cheesey.route.ts
@@ -3,7 +3,6 @@ import {
     getCheeseyByFoodpicIdAndProfileIdController,
     postCheeseyScoreController
 } from "../controllers/cheesey.controller";
-import {getForkByProfileIdAndRestaurantIdController} from "../controllers/fork.controller";
 
 
 export const CheeseyRoute = Router()
@@ -12,4 +11,4 @@ CheeseyRoute.route("/")
     .post(postCheeseyScoreController)
 
 CheeseyRoute.route("/foodpicId/:foodpicId/profileId/:profileId")
-    .get(getForkByProfileIdAndRestaurantIdController)
+    .get(getCheeseyByFoodpicIdAndProfileIdController)
