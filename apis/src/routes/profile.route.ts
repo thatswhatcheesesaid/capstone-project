95f4b51383612900069ea44d28f81fc93a57214f
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-19 09:22:44 -0600

    finalized sprint3

--- apis/src/routes/profile.route.ts
+++ apis/src/routes/profile.route.ts
@@ -1,14 +1,12 @@
 import {Router} from "express";
 import {
     deleteProfileController, getAllProfilesController,
-    getProfileByEmailController,
     getProfileByActivationTokenController,
     getProfileByProfileIdController,
     postProfileController
 } from "../controllers/profile.controller";
 import {deleteProfile} from "../../utils/profile/deleteProfile";
 import {insertProfile} from "../../utils/profile/insertProfile";
-import {selectProfileByEmail} from "../../utils/profile/selectProfileByEmail";
 import {selectProfileByActivationToken} from "../../utils/profile/selectProfileByActivationToken";
 import {selectProfileByProfileId} from "../../utils/profile/selectProfileByProfileId";
 
