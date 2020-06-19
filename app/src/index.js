f9fd4c403724342b9cb653e1ec8ca7ede82c2646
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-19 10:54:04 -0600

    added random files

--- app/src/index.js
+++ app/src/index.js
@@ -15,6 +15,11 @@ import {AboutUsPage} from "./pages/AboutUsPage";
 import {PicturePage} from './pages/PicturePage'
 import {ResetPage} from './pages/ResetPage'
 
+// import reducer from "./store"
+import { Provider } from 'react-redux'
+
+// const store = configureStore({reducer})
+
 const Routing = () => (
   <>
 
