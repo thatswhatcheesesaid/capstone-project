1a7da90cc3b3148d6f014a9f6242049727f3df75
Kevin Adams <kadams34@cnm.edu>
2020-06-24 07:56:40 -0600

    tried adding functionality to cheesey buttons

--- app/src/pages/PicturePage/TwoRandomFoodpics.js
+++ app/src/pages/PicturePage/TwoRandomFoodpics.js
@@ -2,16 +2,33 @@ import React from "react";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
+import { fetchAllFoodpic } from '../../store/foodpic'
+import { useDispatch } from 'react-redux'
+import { AxiosInstance as httpConfig } from 'axios'
 // import Card from "react-bootstrap/Card";
 // import Row from "react-bootstrap/Row";
 
 export function TwoRandomFoodpics({foodpic}) {
+
+	const dispatch = useDispatch()
+
+	const submitCheesey = (values, {resetForm, setStatus}) => {
+		httpConfig.get("/apis/foodpic", values).then(reply => {
+			let {message, type} = reply
+			if (reply.status === 200) {
+				resetForm()
+				dispatch(fetchAllFoodpic())
+
+			}
+			setStatus({message, type})
+		})
+	}
 	return (
 		<>
 			<Col className="pr-5 text-center">
 				<Image fluid width={400} src={foodpic.foodpicUrl} alt={foodpic.foodpicCaption}/>
 				<p>{foodpic.foodpicCaption}</p>
-				<Button variant='flat'><strong>Cheesey</strong></Button>
+				<Button onClick={submitCheesey} variant='flat'><strong>Cheesey</strong></Button>
 			</Col>
 		</>
 	)
