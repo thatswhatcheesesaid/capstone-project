9673af0c132fbf36968fa28804c2f1a7a78e416f
Carl Hernandez <carl.hernandez89@gmail.com>
2020-06-24 11:45:41 -0600

    more work on the fork stuff

--- app/src/pages/PicturePage/TwoRandomFoodpics.js
+++ app/src/pages/PicturePage/TwoRandomFoodpics.js
@@ -4,28 +4,29 @@ import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import { shiftFoodpic } from '../../store/foodpic'
 import { useDispatch } from 'react-redux'
-import { AxiosInstance as httpConfig } from 'axios'
+// import { AxiosInstance as httpConfig } from 'axios'
 // import Card from "react-bootstrap/Card";
 // import Row from "react-bootstrap/Row";
+import {httpConfig} from "../..//utils/http-config";
 
 export function TwoRandomFoodpics({foodpic}) {
 
 	const dispatch = useDispatch()
-
+	const cheesey = {
+		cheeseyFoodpicId: foodpic.foodpicId,
+		cheeseyScore: 1,
+	}
 	const submitCheesey = () => {
-		// const cheesey = {
-		// 	cheeseyFoodpicId: "",
-		// 	cheeseyScore: 1,
-		// }
+
 		dispatch(shiftFoodpic(foodpic))
-		// httpConfig.post("/apis/cheesey", cheesey).then(reply => {
-		// 	let {message, type} = reply
+		httpConfig.post("/apis/cheesey", cheesey).then(reply => {
+			let {message, type} = reply
 		// 	if (reply.status === 200) {
 		// 		resetForm()
 		//
 		// 	}
 		// 	setStatus({message, type})
-		// })
+		})
 	}
 	return (
 		<>
