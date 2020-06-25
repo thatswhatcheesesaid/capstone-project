6d31f6807d51dc5f7c7f37f627d07455b26d37f9
Kevin Adams <kadams34@cnm.edu>
2020-06-24 19:56:18 -0600

    fixed login

--- app/src/pages/SignUpPage/LogIn/LogIn.js
+++ app/src/pages/SignUpPage/LogIn/LogIn.js
@@ -7,50 +7,50 @@ import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 
 import * as Yup from "yup";
-import {Formik} from "formik";
+import { Formik } from "formik";
 
-import {LogInContent} from "./LogInContent";
-import {httpConfig} from "../../../shared/utils/http-config";
-
+import { LogInContent } from "./LogInContent";
+import { httpConfig } from "../../../shared/utils/http-config";
 
 export const LogIn = () => {
-	const logIn = {
-		profileEmail: "",
-		profilePassword: "",
-	};
+  const logIn = {
+    profileEmail: "",
+    profilePassword: "",
+  };
 
-	const validator = Yup.object().shape({
-		profileEmail: Yup.string()
-			.email("Email must be a valid email")
-			.required("Email is required"),
-		profilePassword: Yup.string()
-			.required("Password is required")
-			.min(8, "Password must have at least eight characters"),
-	});
+  const validator = Yup.object().shape({
+    profileEmail: Yup.string()
+      .email("Email must be a valid email")
+      .required("Email is required"),
+    profilePassword: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must have at least eight characters"),
+  });
 
-	const submitLogIn = (values, {resetForm, setStatus}) => {
+  const submitLogIn = (values, {resetForm, setStatus}) => {
 
-		httpConfig.post("/apis/sign-in/", values)
-			.then(reply => {
-					let {message, type} = reply;
+    httpConfig.post("/apis/sign-in/", values)
+      .then(reply => {
+          let {message, type} = reply;
 
-					if (reply.status === 200) {
-						resetForm();
-						setStatus({message, type});
-					}
-					setStatus({message, type});
-				}
-			);
-	};
+            if (reply.status === 200 && reply.headers["authorization"]) {
+              window.localStorage.removeItem("authorization");
+              window.localStorage.setItem("authorization", reply.headers["authorization"]);
+              resetForm();
+            }
+          setStatus({message, type});
+        }
+      );
+  };
 
-	return (
+  return (
 
-		<Formik
-			initialValues={LogIn}
-			onSubmit={submitLogIn}
-			validationSchema={validator}
-		>
-			{LogInContent}
-		</Formik>
-	)
+    <Formik
+      initialValues={LogIn}
+      onSubmit={submitLogIn}
+      validationSchema={validator}
+    >
+      {LogInContent}
+    </Formik>
+  )
 }
